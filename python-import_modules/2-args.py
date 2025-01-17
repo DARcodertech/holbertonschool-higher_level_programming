64603518c6ad982e863f8babc49c92f34a189572
root <root@DESKTOP-GV8C5Q5>
2025-01-17 21:06:13 +0100

    add

---
+++ python-import_modules/2-args.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    import sys
+    num_args = len(sys.argv) - 1
+    if num_args == 0:
+        print("{} arguments.".format(num_args))
+    elif num_args == 1:
+        print("{} arguments:".format(num_args))
+        print("{}: {}".format(1, sys.argv[1]))
+    else:
+        print("{} arguments:".format(num_args))
+        for i in range(1, len(sys.argv)):
+            print("{}: {}".format(i, sys.argv[i]))
