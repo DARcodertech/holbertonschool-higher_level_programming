8150a404d2cb498e68878f571aec9beeb6d5dd9b
root <root@DESKTOP-GV8C5Q5>
2025-01-26 16:45:34 +0100

    add

--- python-test_driven_development/3-say_my_name.py
+++ python-test_driven_development/3-say_my_name.py
@@ -1,5 +1,18 @@
 #!/usr/bin/python3
+"""
+function that print a name
+"""
 def say_my_name(first_name, last_name=""):
+    """
+    prints "my name is <first name> <last name>"
+
+    args:
+        first_name: the first name (should be a string)
+        last_name: the last name (should be a string, optional)
+
+    raises:
+        type error: if first_name or last_name is not a string
+    """
     if not isinstance(first_name, str):
         raise TypeError("first_name must be a string")
     if not isinstance(last_name, str):
