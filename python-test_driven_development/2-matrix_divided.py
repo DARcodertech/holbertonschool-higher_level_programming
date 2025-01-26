fa4ff8a8c841c9c22c52df70d467a05e582e6870
root <root@DESKTOP-GV8C5Q5>
2025-01-26 18:10:47 +0100

    add

--- python-test_driven_development/2-matrix_divided.py
+++ python-test_driven_development/2-matrix_divided.py
@@ -3,16 +3,23 @@
     this function divides all the numbers in a matrix by a number
     it checks for errors and gives a new matrix with rounded results
 """
+
+
 def matrix_divided(matrix, div):
     """
     this divides all numbers in a matrix by div
     """
-    if not isinstance(matrix, list) or not all(isinstance(row, list) for row in matrix):
-        raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
-    
+    if not isinstance(matrix, list) or not all(
+            isinstance(row, list) for row in matrix):
+        raise TypeError(
+            "matrix must be a matrix (list of lists) of integers/floats"
+            )
+
     for row in matrix:
         if not all(isinstance(num, (int, float)) for num in row):
-            raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
+            raise TypeError(
+                "matrix must be a matrix (list of lists) of integers/floats"
+                )
     row_len = [len(row) for row in matrix]
     if len(set(row_len)) > 1:
         raise TypeError("Each row of the matrix must have the same size")
@@ -25,5 +32,5 @@ def matrix_divided(matrix, div):
         new_row = []
         for num in row:
             new_row.append(round(num / div, 2))
-        new_matrix.append(new_row)  
+        new_matrix.append(new_row)
     return new_matrix
