a25e84e46353577307bba7049e3c6c1f909a7206
root <root@DESKTOP-GV8C5Q5>
2025-01-29 15:27:22 +0100

    add

--- python-classes/1-square.py
+++ python-classes/1-square.py
@@ -1,4 +1,10 @@
 #!/usr/bin/python3
+"""
+ class Square that defines a square
+ """
+
+
 class Square:
+    """class Square that defines square"""
     def __init__(self, size):
         self.__size = size
