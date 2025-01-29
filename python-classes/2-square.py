9778ee257bc98a5c18b835b0504000b4f59705ba
root <root@DESKTOP-GV8C5Q5>
2025-01-29 15:31:38 +0100

    add

--- python-classes/2-square.py
+++ python-classes/2-square.py
@@ -1,6 +1,13 @@
 #!/usr/bin/python3
+"""
+a class Square that defines a square
+"""
+
+
 class Square:
+    """a class Square that defines a square"""
     def __init__(self, size=0):
+        """verify if there is error"""
         if not isinstance(size, int):
             raise TypeError("size must be an integer")
         if size < 0:
