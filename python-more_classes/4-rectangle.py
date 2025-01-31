ec703247729565b2827d8df54aeda4d0046894a0
root <root@DESKTOP-GV8C5Q5>
2025-01-31 10:41:32 +0100

    add

--- python-more_classes/4-rectangle.py
+++ python-more_classes/4-rectangle.py
@@ -52,3 +52,7 @@ class Rectangle:
         if self.width == 0 or self.height == 0:
             return ""
         return "\n".join("#" * self.width for i in range(self.height))
+
+    def __repr__(self):
+        """return a rectangle"""
+        return f"Rectangle({self.width}, {self.height})"
