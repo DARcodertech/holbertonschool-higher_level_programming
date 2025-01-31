3f2405483b4d40a0498b82b2a8dcae959634294e
root <root@DESKTOP-GV8C5Q5>
2025-01-31 10:13:58 +0100

    add

---
+++ python-more_classes/2-rectangle.py
@@ -0,0 +1,49 @@
+#!/usr/bin/python3
+"""
+class that do rectangle
+"""
+
+
+class Rectangle:
+    """initialie a rectangle"""
+    def __init__(self, width=0, height=0):
+        self.width = width
+        self.height = height
+
+    @property
+    def width(self):
+        """return the width"""
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        """ verify if there is error"""
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = value
+
+    @property
+    def height(self):
+        """return the height"""
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        """verify if there is error"""
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+        if value < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = value
+
+    def area(self):
+        """return the area"""
+        return self.width * self.height
+
+    def perimeter(self):
+        """return the rectangle"""
+        if self.width == 0 or self.height == 0:
+            return 0
+        return 2 * (self.width + self.height)
