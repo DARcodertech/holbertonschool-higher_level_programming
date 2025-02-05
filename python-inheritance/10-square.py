0708fe7baed991acc393a80af7e5c9e4c381f5dc
root <root@DESKTOP-GV8C5Q5>
2025-02-05 11:39:05 +0100

    add

---
+++ python-inheritance/10-square.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""
+class that is square
+"""
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """class that is square"""
+    def __init__(self, size):
+        """initialize a square"""
+        self.integer_validator("size", size)
+        self.__size = size
+        super().__init__(size, size)
+
+    def area(self):
+        """return a square"""
+        return self.__size ** 2
