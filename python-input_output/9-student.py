c16c42d5042c6ae828fef7fff94b2e0782ae0e16
root <root@DESKTOP-GV8C5Q5>
2025-02-13 17:18:18 +0100

    add

--- python-input_output/9-student.py
+++ python-input_output/9-student.py
@@ -5,12 +5,15 @@ define a student
 
 
 class Student:
+    """define a student"""
     def __init__(self, first_name, last_name, age):
+        """init a student"""
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
     
     def to_json(self):
+        """return a student"""
         return{
             'first_name': self.first_name,
             'last_name': self.last_name,
