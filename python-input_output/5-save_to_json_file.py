2b7381252b70165149cb0dbb787c88fc37d39bf6
root <root@DESKTOP-GV8C5Q5>
2025-02-13 10:55:03 +0100

    add

--- python-input_output/5-save_to_json_file.py
+++ python-input_output/5-save_to_json_file.py
@@ -7,5 +7,5 @@ write an object
 
 def save_to_json_file(my_obj, filename):
     """write an object"""
-    with open(filename, "w") as f:
+    with open(filename, 'w') as f:
         json.dump(my_obj, f)
