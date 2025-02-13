4eb67145209be1457e4c8c2cee9eb2c48b779410
root <root@DESKTOP-GV8C5Q5>
2025-02-13 11:48:06 +0100

    add

---
+++ python-input_output/6-load_from_json_file.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""import a json"""
+import json
+"""
+make an object
+"""
+
+
+def load_from_json_file(filename):
+    """make an object"""
+    with open(filename, 'r') as f:
+        json.load(f)
