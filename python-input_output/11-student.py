345570bed56fde7d011d542f2632d5408e34469a
root <root@DESKTOP-GV8C5Q5>
2025-02-13 17:51:01 +0100

    add

---
+++ python-input_output/11-student.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""
+define a student
+"""
+
+
+class Student:
+    """define a student"""
+    def __init__(self, first_name, last_name, age):
+        """init a student"""
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """return a student"""
+        if attrs is None:
+            return self.__dict__
+        else:
+            return {attr: getattr(self, attr)
+                    for attr in attrs if hasattr(self, attr)}
+
+    def reload_from_json(self, json):
+        """reload from json"""
+        for key, value in json.items():
+            setattr(self, key, value)
