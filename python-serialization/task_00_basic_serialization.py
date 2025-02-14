f84d90d40dcc6ddf8fd469c215f88711a022d084
root <root@DESKTOP-GV8C5Q5>
2025-02-14 15:57:27 +0100

    add

--- python-serialization/task_00_basic_serialization.py
+++ python-serialization/task_00_basic_serialization.py
@@ -4,10 +4,13 @@ basic serialization module
 """
 import json
 """import a json"""
+
+
 def serialize_and_save_to_file(data, filename):
     """serialize and save to file"""
     with open(filename, 'w') as f:
         json.dump(data, f)
+
 
 def load_and_deserialize(filename):
     """load and deserialize"""
