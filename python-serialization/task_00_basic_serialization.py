431bc5dbd5bb393bf1f21c314cc2a40fbd005159
root <root@DESKTOP-GV8C5Q5>
2025-02-14 15:52:39 +0100

    add

--- python-serialization/task_00_basic_serialization.py
+++ python-serialization/task_00_basic_serialization.py
@@ -1,9 +1,15 @@
 #!/usr/bin/env python3
+"""
+basic serialization module
+"""
 import json
+"""import a json"""
 def serialize_and_save_to_file(data, filename):
+    """serialize and save to file"""
     with open(filename, 'w') as f:
         json.dump(data, f)
 
 def load_and_deserialize(filename):
+    """load and deserialize"""
     with open(filename 'r') as f:
         return json.load(f)
