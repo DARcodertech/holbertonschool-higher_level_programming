9c275209d0732476c7eef0b22e672b75b545004b
root <root@DESKTOP-GV8C5Q5>
2025-03-07 17:53:45 +0100

    add

--- SQL_more_queries/6-states.sql
+++ SQL_more_queries/6-states.sql
@@ -1,7 +1,7 @@
 -- make a table and database if it dont exist
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa
 CREATE TABLE IF NOT EXISTS states (
-    id INT,
+    id INT NOT NULL AUTO_INCREMENT,
     name VARCHAR(256),
     PRIMARY KEY (id)
 )
