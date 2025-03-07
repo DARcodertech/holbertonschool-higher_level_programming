21bc071824d861fcbee3e623aff1c875c6b057a8
root <root@DESKTOP-GV8C5Q5>
2025-03-07 09:59:38 +0100

    add

--- SQL_more_queries/1-create_user.sql
+++ SQL_more_queries/1-create_user.sql
@@ -1,2 +1,3 @@
 -- grant privileges and passwords
-GRANT ALL PRIVILEGES ON user_0d_1@localhost IDENTIFIED BY 'user_0d_1_pwd'
+CREATE USER user_0d_1 IF NOT EXIST 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
+GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost' WITH GRANT OPTION
