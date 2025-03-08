5a86f632f30e0d7fb8cd88b2aa89e5837001df4f
root <root@DESKTOP-GV8C5Q5>
2025-03-08 14:51:39 +0100

    add

--- SQL_more_queries/8-cities_of_california_subquery.sql
+++ SQL_more_queries/8-cities_of_california_subquery.sql
@@ -1,9 +1,9 @@
 -- list a citie 
-SELECT id, name
-FROM hbtn_0d_usa.cities
-WHERE state_id = (
-    SELECT id
-    FROM hbtn_0d_usa.states
-    WHERE name = 'California'
+SELECT cities.id, cities.name
+FROM hbtn_0d_usa.cities AS cities
+WHERE cities.state_id = (
+    SELECT states.id
+    FROM hbtn_0d_usa.states AS states
+    WHERE states.name = 'California'
 )
-ORDER BY id ASC;
+ORDER BY cities.id ASC;
