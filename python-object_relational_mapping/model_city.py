b458c67892cd4df02ad90fb0dcda9b76744a203a
root <root@DESKTOP-GV8C5Q5>
2025-03-09 23:36:11 +0100

    add

--- python-object_relational_mapping/model_city.py
+++ python-object_relational_mapping/model_city.py
@@ -7,7 +7,7 @@ from sqlalchemy import Column, Integer, String, ForeignKey
 from model_state import Base
 
 
-class Cities(Base):
+class City(Base):
     """class a state"""
     __tablename__ = 'cities'
     id = Column(Integer, primary_key=True, nullable=False, autoincrement=True)
