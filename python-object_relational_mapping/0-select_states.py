869ee32b4abb9c3ae83d53feb42927907919cbcb
root <root@DESKTOP-GV8C5Q5>
2025-03-09 12:40:33 +0100

    add

---
+++ python-object_relational_mapping/0-select_states.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+"""
+list a state
+"""
+import MySQLdb
+import sys
+
+if __name__ == "__main__":
+    if len(sys.argv) != 4:
+        print("Usage: {} <mysql username> <mysql password> <database name>".format(sys.argv[0]))
+        sys.exit(1)
+    username = sys.argv[1]
+    password = sys.argv[2]
+    database = sys.argv[3]
+    try:
+        db = MySQLdb.connect(
+            host="localhost",
+            port=3306,
+            user=username,
+            passwd=password,
+            db=database
+        )
+        cursor = db.cursor()
+        cursor.execute("SELECT * FROM states ORDER BY id ASC")
+        rows = cursor.fetchall()
+        for row in rows:
+            print(row)
+    except MySQLdb.Error as r:
+        print("MySQL Error: {}".format(e))
+        sys.exit(1)
+    finally:
+        if 'cursor' in locals():
+            cursor.close()
+        if 'db' in locals():
+            db.close()
