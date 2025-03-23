6d69bb0a14def84615b6a26cbf8e055be4d8512b
root <root@DESKTOP-GV8C5Q5>
2025-03-23 22:21:04 +0100

    add

--- python-server_side_rendering/task_02_logic.py
+++ python-server_side_rendering/task_02_logic.py
@@ -14,5 +14,12 @@ def about():
 def contact():
     return render_template('contact.html')
 
+@app.route('/items')
+def items():
+    with open('items.json') as f:
+        data = json.load(f)
+        item_list = data.get("items", [])
+    return render_template('items.html', items=item_list)
+
 if __name__ == '__main__':
     app.run(debug=True, port=5000)
